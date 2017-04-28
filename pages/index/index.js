92442a4a5291202d119b268cb59f0e02fb4ffa85
xiaopaozizi <scryc@qq.com>
2017-04-28 18:09:32 +0800

    add bottom-nav and swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,12 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+     movies:[    
+    {url:'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg'} ,    
+    {url:'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg'} ,    
+    {url:'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg'} ,    
+    {url:'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'}     
+    ] 
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+  
 })
