4279e332ebbedc6d5dddc56722fd7f16f4271f15
xiaopaozizi <scryc@qq.com>
2017-04-30 23:14:05 +0800

    add news/detail page

---
+++ pages/news/detail.js
@@ -0,0 +1,23 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    detail : { 
+      title : '预录入成功', 
+      icon : '../../images/pen.png', 
+      content : '您在2017年1月1号拍照上传成功，数据已预录入IDE系统，请点击查看具体信息', 
+      time : '2017-01-01 10:00:00' 
+    },
+    listData:[
+      {"key":"text1","value":"xxxxxxxxxxxx"},
+      {"key":"text2","value":"xxxxxxxxxxxx"},
+      {"key":"text3","value":"xxxxxxxxxxxx"},
+      {"key":"text4","value":"xxxxxxxxxxxx"},
+      {"key":"text5","value":"xxxxxxxxxxxx"},
+      {"key":"text6","value":"xxxxxxxxxxxx"},
+      {"key":"text7","value":"xxxxxxxxxxxx"},
+    ]
+  },
+  
+})
