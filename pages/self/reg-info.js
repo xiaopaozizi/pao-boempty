31a2cfd1e764d8eac8aa6253517cef514fc01649
xiaopaozizi <scryc@qq.com>
2017-06-28 11:30:41 +0800

    6.28 -m pao-boempty 1st 11:30

---
+++ pages/self/reg-info.js
@@ -0,0 +1,28 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    
+  },
+  // 上传图片
+  upload(){
+    wx.chooseImage({
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: 'http://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            'user': 'test'
+          },
+          success: function (res) {
+            var data = res.data
+            //do something
+          }
+        })
+      }
+    })
+  }
+})
